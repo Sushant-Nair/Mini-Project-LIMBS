d9eee8847869a280c55a6252ee6239a87abcc813
Sushant Manoj Nair <104624359+Sushant-Nair@users.noreply.github.com>
2022-07-04 15:12:33 +0530

    Update 109_111_121_LIMBS_Python-Mini-Project.py

--- 109_111_121_LIMBS_Python-Mini-Project.py
+++ 109_111_121_LIMBS_Python-Mini-Project.py
@@ -7,7 +7,6 @@ role=role.upper()
 #Book details input
 programme = input("Enter the programme in which your book\nmay be found (B. Tech., M. Tech., Ph.D.): ")
 programme=programme.upper()
-
 field = input("Enter the field in which the subject is found: ")
 print("Enter the year relevant to the subject: ")
 if programme=="B.TECH." or programme=="B. TECH.":
@@ -15,62 +14,87 @@ if programme=="B.TECH." or programme=="B. TECH.":
 elif programme=="M.TECH." or programme=="M. TECH.":
     year = int(input("'1' for FY, '2' for SY.\nYear: "))
 elif programme=="PH.D." or programme=="PHD":
-    year = int(input("Enter a number indicating the number of years you have been doing this course.\nYear: "))
-        
+    year = int(input("Enter a number indicating the number of years you have been doing this course.\nYear: "))        
 if role=="FACULTY":
-    print("As a Faculty, You can not access this system!!")
+    print("As a Faculty, you are not required to access this system. You have unlimited access\nto an unlimited number of books for an unlimited period of time.\nPlease visit the library to get a copy of a book you require.")
 elif age<18:
     print("Only people of age 18 and above can access this system.")
 else:
     if programme=="B. TECH.":
-        def btech():
-            if year==1:
-                b_l = open('FY_BTECH_BOOKS.csv', 'r')
-                print(b_l.read())
-            elif year==2:
-                if field=="Computer Engineering" or "COMPS":
-                    b_l = open('SY_BTECH_COMPS.csv', 'r')
-                    print(b_l.read(), end = "\t\t")
-                elif field=="Electronics Engineering" or "ETRX":
-                    b_l = open('SY_BTECH_ETRX.csv', 'r')
-                    print(b_l.read())
-                elif field=="Electronics and Telecommunications Engineering" or "EXTC":
-                    b_l = open('SY_BTECH_EXTC.csv', 'r')
-                    print(b_l.read())
-                elif field=="Information Technology" or "IT":
-                    b_l = open('SY_BTECH_IT.csv', 'r')
-                    print(b_l.read())
-                elif field=="Mechanical Engineering" or "MECH":
-                    b_l = open('SY_BTECH_MECH.csv', 'r')
-                    print(b_l.read())
-            elif year==3:
-                print("Not Available!")
-            elif year==4:
-                print("Not Available!")
-            else:
-                print("Sorry! Wrong year has been entered!")
         btech()
     elif programme=="M. TECH.":
-        def mtech():
-            print("Not Available!")        
-        mtech()
+        mtech():
     elif programme=="PH. D.":
-        def phd():
-            print("Not Available!")
-        phd()
+        phd():
     else:
         print("The entered programme does not exist.")
-
-#search algorithm for books - use in-built function
-#file handling
-b_i = open('BOOKS_ISSUE.csv', '\ta')
-b_i.write(input("Enter the subject of the book which you want to issue: "))
-b_i.close()
-b_i = open('BOOKS_ISSUE.csv', 'r')
-print(b_i.read())
-from tkinter.tix import INTEGER
-
+def btech():
+    if year==1:
+        b_l = open('FY_BTECH_BOOKS.csv', 'r')
+        for i in b_l.read().split(","):
+            print(i,end="\t")
+    elif year==2:
+        if field=="Computer Engineering" or "COMPS":
+            b_l = open('SY_BTECH_COMPS.csv', 'r')
+            print(b_l.read())
+        elif field=="Electronics Engineering" or "ETRX":
+            b_l = open('SY_BTECH_ETRX.csv', 'r')
+            print(b_l.read())
+        elif field=="Electronics and Telecommunications Engineering" or "EXTC":
+            b_l = open('SY_BTECH_EXTC.csv', 'r')
+            print(b_l.read())
+        elif field=="Information Technology" or "IT":
+            b_l = open('SY_BTECH_IT.csv', 'r')
+            print(b_l.read())
+        elif field=="Mechanical Engineering" or "MECH":
+            b_l = open('SY_BTECH_MECH.csv', 'r')
+            print(b_l.read())
+    elif year==3:
+            print("Not Available!")
+    elif year==4:
+            print("Not Available!")
+    else:
+            print("Sorry! Wrong year has been entered!")
+def mtech():
+    print("Not Available at the moment!")
+def phd():
+    print("Not Available at the moment!")
+#issue book
+def issue():
+    subject = input("Enter the subject of the book which you want to issue: ")
+    b_i = open('BOOKS_ISSUE.csv', 'a')
+    b_i.write(subject)
+    b_i.write("\n")
+    b_i.close()
+    b_i = open('BOOKS_ISSUE.csv', 'r')
+    print(b_i.read())
+    b_i.close()
+    choice()
+c = 0
+b_i = open('BOOKS_ISSUE.csv', 'r+')
+b_i.seek(0)
+b_i.truncate()
+def choice():
+    global c
+    ch = int(input("Enter '1' if you want to issue a book, otherwise enter any other character\nChoice: "))
+    if ch==1:
+        c = c+1
+        if c<=3:
+            print("hello")
+            issue()
+        else:
+            print("Sorry! No more than three books can be issued.") 
+            b_i = open('BOOKS_ISSUE.csv', 'r')
+            print("The list of books issued during this session is as follows: ")
+            print(b_i.read())
+    else:
+        b_i = open('BOOKS_ISSUE.csv', 'r')
+        print("The list of books issued during this session is as follows: ")
+        print(b_i.read())
+        exit()
+choice()
 #calculating fine
+from tkinter.tix import INTEGER
 iss_date=input("Enter issue date")
 k=iss_date.split("-")
 if (len(k)==3):
@@ -106,8 +130,3 @@ if (len(k)==3):
                   if return_date - issue_date >= 7:
                     print("Interest charged rs 50.")
                     quit()
-
-
-                
-
-
